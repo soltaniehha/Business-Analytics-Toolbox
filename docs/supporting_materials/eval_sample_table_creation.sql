60bf7e5d0c2499590c164027a67190a4af058579
Mohammad Soltanieh-ha <soltaniehha.m@gmail.com>
2019-08-18 21:23:06 -0400

    suplm material added

---
+++ docs/supporting_materials/eval_sample_table_creation.sql
@@ -0,0 +1,25 @@
+-- This query will create a table consisting of sample evaluation data points for logistic regression.
+CREATE OR REPLACE TABLE public.ecommerce_eval_sample
+AS
+SELECT
+  * EXCEPT(fullVisitorId)
+FROM
+
+  # features
+  (SELECT
+    fullVisitorId,
+    IFNULL(totals.bounces, 0) AS bounces,
+    IFNULL(totals.timeOnSite, 0) AS time_on_site
+  FROM
+    `data-to-insights.ecommerce.web_analytics`
+  WHERE
+    totals.newVisits = 1
+    AND date BETWEEN '20170501' AND '20170630') # eval on 2 months
+  JOIN
+  (SELECT
+    fullvisitorid,
+    IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
+  FROM
+      `data-to-insights.ecommerce.web_analytics`
+  GROUP BY fullvisitorid)
+  USING (fullVisitorId)
